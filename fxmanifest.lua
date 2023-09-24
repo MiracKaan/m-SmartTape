2ce7e143878d45d048ebfea851449d90a256fc3d
MirageWasHere <63926390+MiracKaan@users.noreply.github.com>
2023-09-24 12:16:42 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game 'gta5'
+autor 'Mirage' -- Bu script mirage tarafından editlenmiştir.
+-- Bu script LAPD ekibi için özel olarak editlenmiştir.
+
+client_script 'Client.net.dll'
+
+server_script 'Server.net.dll'
+
+files {
+    'NativeUI.dll',
+    'SharpConfig.dll',
+    'config.ini',
+    'stream/props.ytyp',
+    'stream/prop_cordon_tape.ydr',
+    'stream/prop_cordon_tape.ytd',
+    'stream/prop_fire_tape.ydr',
+    'stream/prop_fire_tape.ytd',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/props.ytyp'
